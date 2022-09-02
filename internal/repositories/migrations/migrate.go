4d516b5ec095ee6a654a160aca9750da0ae0e825
徐超越 <993921@qq.com>
2022-09-02 23:09:02 +0800

    init(db-supported) - 数据库迁移优化

--- internal/repositories/migrations/migrate.go
+++ internal/repositories/migrations/migrate.go
@@ -10,21 +10,29 @@ type Migration interface {
 	Down(db *gorm.DB)
 }
 
+var migrations []Migration
+
+func init() {
+	// 迁移对象必需按从旧到新的顺序添加
+	migrations = []Migration{
+		&Migration20220831{},
+	}
+}
+
 // Migrate 数据库变更同步
 func Migrate() {
 	db := conf.GetDbClient()
 
-	var up Migration
-
-	up = &Migration20220831{}
-	up.Up(db)
+	for i := 0; i < len(migrations); i++ {
+		migrations[i].Up(db)
+	}
 }
 
+// Rollback 数据库变更回滚
 func Rollback() {
 	db := conf.GetDbClient()
 
-	var up Migration
-
-	up = &Migration20220831{}
-	up.Down(db)
+	for i := len(migrations) - 1; i >= 0; i-- {
+		migrations[i].Down(db)
+	}
 }
--- internal/repositories/migrations/migrate_test.go
+++ internal/repositories/migrations/migrate_test.go
@@ -1,13 +1,57 @@
 package migrations
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+	"testing"
+)
 
+var sortedUp, sortedDown []int
+
+type migration001 struct {
+}
+
+func (m *migration001) Up(db *gorm.DB) {
+	println("[up]001")
+	sortedUp = append(sortedUp, 1)
+}
+
+func (m *migration001) Down(db *gorm.DB) {
+	println("[down]001")
+	sortedDown = append(sortedDown, 1)
+}
+
+type migration002 struct {
+}
+
+func (m *migration002) Up(db *gorm.DB) {
+	println("[up]002")
+	sortedUp = append(sortedUp, 2)
+}
+
+func (m *migration002) Down(db *gorm.DB) {
+	println("[down]002")
+	sortedDown = append(sortedDown, 2)
+}
+
+func init() {
+	sortedUp = make([]int, 0)
+	sortedDown = make([]int, 0)
+
+	migrations = []Migration{
+		&migration001{},
+		&migration002{},
+	}
+}
 func TestMigrate(t *testing.T) {
 	if testing.Short() {
 		t.Skip("debugger only")
 	}
 
 	Migrate()
+
+	assert.Equal(t, 1, sortedUp[0])
+	assert.Equal(t, 2, sortedUp[1])
 }
 
 func TestRollback(t *testing.T) {
@@ -16,4 +60,7 @@ func TestRollback(t *testing.T) {
 	}
 
 	Rollback()
+
+	assert.Equal(t, 2, sortedDown[0])
+	assert.Equal(t, 1, sortedDown[1])
 }
