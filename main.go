d0c8c8374558edfcd2b638000c2d4745fb036b7d
徐超越 <993921@qq.com>
2022-09-29 10:06:16 +0800

    run mode

--- main.go
+++ main.go
@@ -2,38 +2,54 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"go-to-cloud/conf"
 	"go-to-cloud/internal/routers"
+	"os"
 	"strings"
 )
 
-var aType = flag.String("type", "agent", "运行方式: agent / web")
-var aPort = flag.String("port", ":8080", "端口")
+var aType = flag.String("type", "", "运行方式: agent / web")
+var aPort = flag.String("port", "", "端口")
 
-var confFile *string
+// runMode 获取运行方式
+// @bool: true:web; false: agent
+// @string: 端口
+func runMode() (bool, string) {
+	// 优先读取命令行参数，其次使用go env，最后使用默认值
+	flag.Parse()
+
+	if len(*aType) == 0 {
+		*aType = os.Getenv("type")
+	}
+
+	if len(*aPort) == 0 {
+		*aPort = os.Getenv("port")
+	}
+
+	if len(*aType) == 0 {
+		*aType = "web"
+	}
+
+	if len(*aPort) == 0 {
+		*aPort = ":8080"
+	}
+
+	if !strings.HasPrefix(*aPort, ":") {
+		*aPort = ":" + *aPort
+	}
+
+	return strings.EqualFold(*aType, "web"), *aPort
+}
 
 // @securityDefinitions.apikey JWT
 // @in header
 // @name Authorization
 func main() {
-	flag.Parse()
-
-	if strings.EqualFold("web", *aType) {
-		if len(*aPort) == 0 {
-			*aPort = ":8080"
-		}
+	runType, port := runMode()
+	if runType {
 		// web模式运行
-		_ = routers.SetRouters().Run(*aPort)
+		_ = routers.SetRouters().Run(port)
 	} else {
 		// TODO: k8s agent模式运行
 		// _ = agent.Startup()
 	}
-}
-
-func init() {
-	envName := conf.Environment.GetEnvName()
-
-	var tmp string
-	tmp, confFile = fmt.Sprintf("conf/appsettings.%s.yaml", *envName), &tmp
 }
