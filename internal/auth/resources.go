c58a96938b4b517ec0c88567a1a289ef7579e7bb
徐超越 <993921@qq.com>
2023-03-19 09:50:32 +0800

    wip: authz

--- internal/auth/resources.go
+++ internal/auth/resources.go
@@ -2,23 +2,30 @@ package auth
 
 import (
 	"go-to-cloud/internal/models"
-	"strconv"
 )
 
-func GroupPolicies() [][]string {
-	return [][]string{
+var groupPolicies [][]string
+var resourcePolicies [][][]string
+
+func init() {
+	groupPolicies = [][]string{
 		{string(models.Root), "*"},
 		{string(models.Ops), string(models.Dev)},
 		{string(models.Dev), string(models.Guest)},
 	}
+
+	resourcePolicies = [][][]string{
+		//{{string(models.Ops), strconv.Itoa(int(models.PodDelete)), "RESOURCE"}},
+		//
+		//{{string(models.Ops), strconv.Itoa(int(models.PodDelete)), "RESOURCE"}},
+		//{{string(models.Ops), strconv.Itoa(int(models.PodShell)), "RESOURCE"}},
+		//{{string(models.Ops), strconv.Itoa(int(models.PodViewLog)), "RESOURCE"}},
+	}
+}
+func GroupPolicies() [][]string {
+	return groupPolicies
 }
 
 func ResourcePolicies() [][][]string {
-	return [][][]string{
-		{{string(models.Ops), strconv.Itoa(int(models.PodDelete)), "RESOURCE"}},
-
-		{{string(models.Ops), strconv.Itoa(int(models.PodDelete)), "RESOURCE"}},
-		{{string(models.Ops), strconv.Itoa(int(models.PodShell)), "RESOURCE"}},
-		{{string(models.Ops), strconv.Itoa(int(models.PodViewLog)), "RESOURCE"}},
-	}
+	return resourcePolicies
 }
