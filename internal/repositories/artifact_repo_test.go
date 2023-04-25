5fe8d705f159f45f11c3ab88f79ca2135f231382
徐超越 <993921@qq.com>
2023-04-25 21:52:33 +0800

    untitests

--- internal/repositories/artifact_repo_test.go
+++ internal/repositories/artifact_repo_test.go
@@ -81,3 +81,38 @@ func TestBindK8sRepo(t *testing.T) {
 	//assert.Equal(t, orgName, a[0].Name)
 	//assert.Equal(t, orgRemark, a[0].Remark)
 }
+
+func TestGetArtifactRepoByID(t *testing.T) {
+	if err := os.Setenv("UnitTestEnv", "1"); err != nil {
+		t.Skip("skipped due to CI")
+	} else {
+		defer func() {
+			os.Unsetenv("UnitTestEnv")
+		}()
+		prepareDb()
+	}
+
+	err := CreateOrg(&orgName, &orgRemark)
+	assert.NoError(t, err)
+
+	err = BindArtifactRepo(&artifact.Artifact{
+		Testing: artifact.Testing{
+			Type:       artifact.Maven,
+			IsSecurity: true,
+			Url:        "a",
+			User:       "b",
+			Password:   "c",
+		},
+		Name:   orgName,
+		Remark: orgRemark,
+	}, 1, []uint{1})
+	assert.NoError(t, err)
+
+	url, account, password, isSecurity, origin, err := GetArtifactRepoByID(1)
+	assert.NoError(t, err)
+	assert.Equal(t, "a", *url)
+	assert.Equal(t, "b", *account)
+	assert.Equal(t, "c", *password)
+	assert.Equal(t, true, isSecurity)
+	assert.EqualValues(t, artifact.Maven, origin)
+}
